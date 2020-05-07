e551629a05fad199d42a04f482fc27f73388eb57
xenophon123 <43081788+xenophon123@users.noreply.github.com>
2020-05-06 22:20:11 -0400

    1

--- client.py
+++ client.py
@@ -9,6 +9,8 @@ import sys
 import threading
 from time import sleep
 
+SEND_BUFFER = 4096
+
 # The Mad audio library we're using expects to be given a file object, but
 # we're not dealing with files, we're reading audio data over the network.  We
 # use this object to trick it.  All it really wants from the file object is the
@@ -35,8 +37,48 @@ class mywrapper(object):
 # it too!
 def recv_thread_func(wrap, cond_filled, sock):
     while True:
-        # TODO
-        pass
+        data = sock.recv(SEND_BUFFER)
+        if data is None or len(data) == 0:
+            print "Client Error in Receiving Data from Server"
+            print "Close the recv thread"
+            break
+        data_lst = data.split("\n")
+        if len(data_lst) < 4:
+            print "Invalid Framing"
+            continue
+        try:
+            _, status = data_lst[0].split(":")
+            _, command = data_lst[1].split(":")
+        except ValueError:
+            print "Invalid Framing for TYPE/COMMAND"
+            continue
+        if status == "RES/INVALID":
+            print "Invalid ", command
+            continue
+        if command == "LIST":
+            if data_lst[3] == "":
+                print "Empty Song List"
+                continue
+            print "Song List:"
+            for i in range(3, len(data_lst)):
+                print data_lst[i]
+        elif command == "STOP":
+            print "Stop playing the song"
+            cond_filled.acquire()
+            wrap.data = ""
+            wrap.mf = None
+            cond_filled.release()
+        elif command == "PLAY":
+            print "Begin playing the song"
+            cond_filled.acquire()
+            wrap.data = ""
+            wrap.mf = mad.MadFile(wrap)
+            cond_filled.notify()
+            cond_filled.release()
+        elif command == "PLAYING":
+            cond_filled.acquire()
+            wrap.data += "\n".join(data_lst[3:])
+            cond_filled.release()
 
 
 # If there is song data stored in the wrapper object, play it!
@@ -45,12 +87,45 @@ def recv_thread_func(wrap, cond_filled, sock):
 # using it too!
 def play_thread_func(wrap, cond_filled, dev):
     while True:
-        """
-        TODO
-        example usage of dev and wrap (see mp3-example.py for a full example):
+        buf = None
+        cond_filled.acquire()
+        if wrap.mf is None:
+            cond_filled.wait()
         buf = wrap.mf.read()
+        cond_filled.release()
+        if buf is None:
+            continue
         dev.play(buffer(buf), len(buf))
-        """
+
+
+def send_list_command(socket):
+    data = "TYPE:REQ\nCOMMAND:LIST\n\n"
+    try:
+        socket.sendall(data)
+    except:
+        print "Client Error in Sending Data to Server"
+        print "Client Quit"
+        sys.exit(2)
+
+
+def send_play_command(socket, song_id):
+    data = "TYPE:REQ\nCOMMAND:PLAY\nPARAMETER:{0}\n\n".format(song_id)
+    try:
+        socket.sendall(data)
+    except:
+        print "Client Error in Sending Data to Server"
+        print "Client Quit"
+        sys.exit(2)
+
+
+def send_stop_command(socket):
+    data = "TYPE:REQ\nCOMMAND:STOP\n\n"
+    try:
+        socket.sendall(data)
+    except:
+        print "Client Error in Sending Data to Server"
+        print "Client Quit"
+        sys.exit(2)
 
 
 def main():
@@ -99,14 +174,17 @@ def main():
         else:
             cmd = line
 
-        # TODO: Send messages to the server when the user types things.
+        # Send messages to the server when the user types things.
         if cmd in ['l', 'list']:
+            send_list_command(sock)
             print 'The user asked for list.'
 
         if cmd in ['p', 'play']:
+            send_play_command(sock, args) # TODO: 1) play the same song twice 
             print 'The user asked to play:', args
 
         if cmd in ['s', 'stop']:
+            send_stop_command(sock)
             print 'The user asked for stop.'
 
         if cmd in ['quit', 'q', 'exit']:
--- server.py
+++ server.py
@@ -5,44 +5,117 @@ import socket
 import struct
 import sys
 from threading import Lock, Thread
+from collections import deque
 
 
 QUEUE_LENGTH = 10
 SEND_BUFFER = 4096
+SONG_INFO = []
 
 # per-client struct
 class Client:
-    def __init__(self):
+    def __init__(self, conn):
         self.lock = Lock()
+        self.conn = conn
+        self.data_queue = deque()
+        self.song_id = -1
+        self.music_file = None
+        self.already_sent = 0
+        self.is_updated = False
 
 
-# TODO: Thread that sends music and lists to the client.  All send() calls
+# Thread that sends music and lists to the client.  All send() calls
 # should be contained in this function.  Control signals from client_read could
 # be passed to this thread through the associated Client object.  Make sure you
 # use locks or similar synchronization tools to ensure that the two threads play
 # nice with one another!
 def client_write(client):
+    while True:
+        client.lock.acquire()
+        while len(client.data_queue) != 0:
+            try:
+                client.conn.sendall(client.data_queue.popleft())
+            except:
+                print "Server Error in Sending Data to Client"
+                print "Close the write thread"
+                return
+        client.lock.release()
 
-
-# TODO: Thread that receives commands from the client.  All recv() calls should
+        client.lock.acquire()
+        if client.is_updated:
+            client.is_updated = False
+            f = open(SONG_INFO[2] + "/" + SONG_INFO[0][client.song_id], "r")
+            client.music_file = f.read()
+            client.already_sent = 0
+            f.close()
+        if client.music_file != None:
+            if client.already_sent >= len(client.music_file):
+                client.music_file = None
+            else:
+                header = "TYPE:RES/OK\nCOMMAND:PLAYING\n\n"
+                body_len = SEND_BUFFER - len(header)
+                data = client.music_file[client.already_sent:client.already_sent+body_len]
+                client.already_sent += body_len
+                client.data_queue.append(header + data)
+        client.lock.release()
+        
+# Thread that receives commands from the client.  All recv() calls should
 # be contained in this function.
 def client_read(client):
-
+    while True:
+        data = client.conn.recv(SEND_BUFFER)
+        if data == "":
+            print "Server Error in Receiving Data from Client"
+            print "Close the read thread"
+            break
+        print "\nserver receives: \n", data
+        data_lst = data.split("\n")
+        if len(data_lst) < 4:
+            print "Invalid Framing"
+            continue
+        try:
+            _, command = data_lst[1].split(":")
+        except ValueError:
+            print "Invalid Framing for COMMAND"
+            continue
+        if command == "LIST":
+            client.lock.acquire()
+            client.data_queue.appendleft("TYPE:RES/OK\nCOMMAND:LIST\n\n" + SONG_INFO[1])
+            client.lock.release()
+        elif command == "STOP":
+            client.lock.acquire()
+            client.music_file = None 
+            client.data_queue.append("TYPE:RES/OK\nCOMMAND:STOP\n\n")
+            client.lock.release()
+        elif command == "PLAY":
+            song_id = -1
+            try:
+                song_id = int(data_lst[2].split(":")[1])
+            except ValueError:
+                print "Invalid Framing for PARAMETER"
+                continue
+            if song_id >= len(SONG_INFO[0]) or song_id < 0:
+                print "Invalid Song ID"
+                continue
+            client.lock.acquire()
+            client.data_queue.append("TYPE:RES/OK\nCOMMAND:PLAY\n\n")
+            client.song_id = song_id
+            client.is_updated = True
+            client.lock.release()
 
 def get_mp3s(musicdir):
     print("Reading music files...")
     songs = []
-
     for filename in os.listdir(musicdir):
         if not filename.endswith(".mp3"):
             continue
-
-        # TODO: Store song metadata for future use.  You may also want to build
-        # the song list once and send to any clients that need it.
-
-        songs.append(None)
-
+        songs.append(filename)
+    songs_str = []
+    for i in range(len(songs)):
+        song_str = "[" + str(i) + "] " + songs[i]
+        songs_str.append(song_str)
     print("Found {0} song(s)!".format(len(songs)))
+    return songs, "\n".join(songs_str)
 
 def main():
     if len(sys.argv) != 3:
@@ -51,16 +124,24 @@ def main():
         sys.exit("Directory '{0}' does not exist".format(sys.argv[2]))
 
     port = int(sys.argv[1])
-    songs, songlist = get_mp3s(sys.argv[2])
+    songs, songs_str = get_mp3s(sys.argv[2])
+    SONG_INFO.append(songs)
+    SONG_INFO.append(songs_str)
+    SONG_INFO.append(sys.argv[2])
     threads = []
 
-    # TODO: create a socket and accept incoming connections
+    # Create a socket and accept incoming connections
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.bind(("127.0.0.1", port))
+    s.listen(QUEUE_LENGTH)
+
     while True:
-        client = Client()
-        t = Thread(target=client_read, args=(client))
+        conn, _ = s.accept()
+        client = Client(conn)
+        t = Thread(target=client_read, args=(client,))
         threads.append(t)
         t.start()
-        t = Thread(target=client_write, args=(client))
+        t = Thread(target=client_write, args=(client,))
         threads.append(t)
         t.start()
     s.close()
